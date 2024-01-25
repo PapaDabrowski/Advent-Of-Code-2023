024f068d82f28e4873a4ad8135b0145ee9692dd4
Piotr Dąbrowski <39497880+PapaDabrowski@users.noreply.github.com>
2024-01-25 17:03:34 +0100

    Update Solver.kt

--- AoC-6/src/main/kotlin/Solver.kt
+++ AoC-6/src/main/kotlin/Solver.kt
@@ -3,6 +3,7 @@ import kotlin.math.sqrt
 
 class Solver {
     private var inputList = mutableListOf<String>()
+
     fun firstSolution(): Int {
         var result = 1
         var listOfRecords = inputList[0].substringAfter(": ").split(" ").filter { !it.isNullOrEmpty() }.map { it.toInt() }.toMutableList()
@@ -14,11 +15,13 @@ class Solver {
         }
         return result
     }
+
     fun secondSolution(): Int {
         var elem = Pair(inputList[0].substringAfter(": ").filter { it.isDigit() }.toLong(), inputList[1].substringAfter(": ").filter { it.isDigit() }.toLong())
         var delta= elem.first * elem.first - 4 * elem.second
         return ((- elem.first - sqrt(delta.toDouble())) / ( -2 )).toInt() - ((- elem.first + sqrt(delta.toDouble())) / ( -2 )).toInt()
     }
+
     fun readFileLineByLine(fileName : String) = File(fileName).forEachLine {
         inputList.add(it)
     }
